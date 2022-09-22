dc0577db3d63f9a457d9844c4061da69e0cc825c
Amir Hajibabaei <ah2281@wildcat>
2022-09-22 10:45:47 +0100

    rocksalt

---
+++ surfsci/build/rocksalt.py
@@ -0,0 +1,17 @@
+# +
+from ase.spacegroup import crystal
+from ase import Atoms
+
+__all__ = ["rocksalt"]
+
+
+def rocksalt(
+    a: float, symbols: tuple[str, str], size: tuple[int, int, int] = (1, 1, 1)
+) -> Atoms:
+    atoms = crystal(
+        symbols=symbols,
+        basis=((0, 0, 0), (0.5, 0.5, 0.5)),
+        spacegroup=225,
+        cellpar=(a, a, a, 90, 90, 90),
+    )
+    return atoms
