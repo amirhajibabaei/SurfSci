7ce62193a05fbbc5fdb1042c10030690c0ddca42
Amir Hajibabaei <ah2281@wildcat>
2022-09-27 18:08:53 +0100

    right-angle cell transforms

--- surfsci/build/transform.py
+++ surfsci/build/transform.py
@@ -1,10 +1,60 @@
 # +
+from math import pi
+
+import numpy as np
+
 from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.transformations.transformation_abc import AbstractTransformation
 from pymatgen.transformations.standard_transformations import (
     ConventionalCellTransformation,
 )
+
 from ase import Atoms
+
+from surfsci.geometry.rotations import AxisRotation, PrismRotation
+from surfsci.geometry.equallity import locally_identical
+
+
+def right_angle_cell(atoms: Atoms) -> Atoms:
+    """
+    Tries to convert a prism cell into cubic by cutting
+    and moving the corners. Note that this will NOT
+    preserve the indexwise neighborhood relations.
+    That means that (i, j) distance may change
+    in the generated structure. But this could still
+    be usefull if the local chemistry remains the same
+    for all atoms. As certain translational symmetry
+    is required, this may not allways be possible.
+    Thus an error will be raised if the transformation
+    is not possible.
+
+    It returns an Atoms object without modifying the input.
+
+    """
+    # i. test if the input is qualified
+    assert all(atoms.pbc)
+    par = atoms.cell.cellpar()
+    if not np.allclose(par[3:], [90, 90, 60]):
+        raise RuntimeError("Only cells with 90, 90, 60 angles are supported for now!")
+        # TODO: generalize
+
+    # ii. create
+    rot1 = PrismRotation(atoms.cell)
+    rot2 = AxisRotation((0, 0, 1), pi / 6)
+    right = Atoms(
+        numbers=atoms.numbers,
+        positions=rot2(rot1(atoms.positions)),
+        cell=rot2(rot1(atoms.cell)),
+        pbc=atoms.pbc,
+    )
+    assert np.allclose(right.cell[2, :2], 0)
+    right.cell[0, 1] = 0
+    right.wrap()
+
+    # iii. test
+    if not locally_identical(atoms, right):
+        raise RuntimeError("Transformation is not possible!")
+    return right
 
 
 def apply_transformation(tr: AbstractTransformation, atoms: Atoms) -> Atoms:
