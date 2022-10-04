11c2e22949e9d0ed578c37df512e54d7edc9b2be
Amir Hajibabaei <ah2281@wildcat>
2022-10-04 12:28:45 +0100

    find angles

--- surfsci/assertions.py
+++ surfsci/assertions.py
@@ -2,6 +2,17 @@
 import numpy as np
 
 
+def in_range(f: float, a: float, b: float) -> bool:
+    if f >= a and f <= b:
+        return True
+    else:
+        return False
+
+
+def in_range_rtol(f: float, g: float, rtol: float) -> bool:
+    return in_range(f, g * (1 - rtol), g * (1 + rtol))
+
+
 def array_is_tril(arr: np.ndarray) -> bool:
     return np.allclose(arr[np.triu_indices_from(arr, k=1)], 0.0)
 
--- surfsci/geometry/topology.py
+++ surfsci/geometry/topology.py
@@ -1,10 +1,12 @@
 # +
+from itertools import combinations
 from typing import Sequence
 
 import numpy as np
 from ase import Atoms
 from ase.neighborlist import NeighborList
 
+from ..assertions import in_range_rtol
 from .nlutil import get_neighbors
 
 
@@ -50,11 +52,50 @@ def find_bonds(
         for t, (ba, bb, bd) in enumerate(bond_types):
             if a.symbol == ba:
                 for j, d in nei:
-                    if (
-                        atoms[j].symbol == bb
-                        and d > bd * (1 - rtol)
-                        and d < bd * (1 + rtol)
-                    ):
+                    if atoms[j].symbol == bb and in_range_rtol(d, bd, rtol):
                         bonds_a.append((t, j))
         bonds.append(bonds_a)
     return bonds
+
+
+def find_angles(
+    atoms: Atoms,
+    bonds: list[list[tuple[int, int]]],
+    angle_types: Sequence[tuple[int, int, float]],
+    rtol: float = 1.1,
+):
+    """
+    Args:
+        atoms:        ase.Atoms object
+        bonds:        output of find_bonds
+        angle_types:  a sequence of angle types e.g.
+                        [(0, 0, 1.), ...]
+                      where the first two are indices of two bond
+                      types (which is used for creating the bonds)
+                      and the third is a float indicating the angle.
+        rtol:         relative tolerance for angle
+
+    Returns:
+        a list of angles for each atom.
+        [
+            [(t1, j1, k1), ...],   <- angle j1--0--k1
+            [(t2, j2, k2), ...],   <- bonds j2--1--k2
+                 ...                      etc.
+        ]
+        where an angle is encoded by tuple of (t, j, k) in which
+        t is the type of angle (index in angle_types) and
+        j,k are the indices of two other atoms forming the angle.
+
+    """
+    angles = []
+    for a, bonds_a in enumerate(bonds):
+        angles_a = []
+        for b1, b2 in combinations(bonds_a, 2):
+            type_ = {b1[0], b2[0]}  # set
+            for t, (bt1, bt2, angle) in enumerate(angle_types):
+                if {bt1, bt2} == type_:  # set equality
+                    angle_ = atoms.get_angle(b1[1], a, b2[1])
+                    if in_range_rtol(angle, angle_, rtol):
+                        angles_a.append((t, b1[1], b2[1]))
+        angles.append(angles_a)
+    return angles
